c65c2b6d6902fa57aac97e7490f2a6a9f3acd94c
shimashimau <shuriken233@gmail.com>
2022-02-23 11:30:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,34 @@
-let h3 = document.querySelector('h3');
-let options = document.querySelectorAll('.option');
-let optionsDiv = document.querySelector('.options');
-let h2 = document.querySelector('h2');
-let paragraph = document.querySelector('p');
+function mainCode(quiz) {
+    let h3 = document.querySelector('h3');
+    let options = document.querySelectorAll('.option');
+    let optionsDiv = document.querySelector('.options');
+    let h2 = document.querySelector('h2');
+    let paragraph = document.querySelector('p');
 
 
-
-function askQuestion() {
-    let currentQuestion = quiz.getCurrentQuestion();
-    paragraph.innerHTML = 'Pitanje ' + quiz.questionCounter();
-    h3.innerHTML = currentQuestion.text;
-    for (let i = 0; i < options.length; i++) {
-        let option =  currentQuestion.options[i];
-        options[i].innerHTML = option;
-        options[i].onclick = checkAnswer;
+    function askQuestion() {
+        let currentQuestion = quiz.getCurrentQuestion();
+        h3.innerHTML = currentQuestion.text;
+        paragraph.innerHTML = quiz.questionsCounter();
+        for (let i = 0; i < options.length; i++) {
+            let option = currentQuestion.options[i];
+            options[i].innerHTML = option;
+            options[i].onclick = checkAnswer;
+        }
     }
-}
 
-askQuestion();
+    askQuestion();
 
-function checkAnswer() {
-    quiz.checkAnswer(this.innerHTML);
-    if (!quiz.isEnd()) {
-        askQuestion();
-    } else {
-        optionsDiv.style.display = 'none';
-        h3.style.display = 'none';
-        paragraph.style.display = 'none';
-        h2.innerHTML = 'Osvojili ste ' + quiz.score + ' od mogucih ' + quiz.QuestionsTotalPoints() + ' poena.';
-        h2.style.display = 'block';
+    function checkAnswer() {
+        quiz.checkAnswer(this.innerHTML);
+        if (!quiz.isEnd()) {
+            askQuestion();
+        } else {
+            optionsDiv.style.display = 'none';
+            h3.style.display = 'none';
+            paragraph.style.display = 'none';
+            h2.innerHTML = 'You scored ' + quiz.score + ' out of ' + quiz.questionsTotalPoints() + ' points';
+            h2.style.display = 'block';
+        }
     }
 }
